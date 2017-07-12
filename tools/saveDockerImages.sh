edf719cb2df076f25079de17c201328c07de7d29
Xiao An <hac@zju.edu.cn>
2017-07-12 20:31:15 +0800

    enable saving Docker images one by one
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- tools/saveDockerImages.sh
+++ tools/saveDockerImages.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
-docker images | tail -n +2 |\
-while read line
-do
-    imageName=`echo $line | cut -d ' ' -f 1`
-    imageVersion=`echo $line | cut -d ' ' -f 2`
-    imageId=`echo $line | cut -d ' ' -f 3`
+list=`docker images | tail -n +2`
+IFS=$'\n'
+for line in $list; do
+    line=`echo $line | tr -s ' '`
+    imageName=`echo $line | cut -d ' ' -f1`
+    imageVersion=`echo $line | cut -d ' ' -f2`
+    imageId=`echo $line | cut -d ' ' -f3`
     shortName=${imageName##*/}
-    echo -n $shortName
-    [[ -f $shortName.tar ]] && echo "$shortName.tar already exists" || eval "echo '' && docker save $imageId > $shortName.tar" 
+    read -p "save $shortName? y/n: " cmd
+    if [[ $cmd == "y" ]]; then
+        [[ -f $shortName.tar ]] && echo "$shortName.tar already exists" || eval "echo '' && docker save $imageId > $shortName.tar" 
+    fi
 done
